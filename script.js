875dfc8da554bb6160a1e48a756cde016d45d261
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-06 01:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-// Complete the js code
-function Car(make, model) {}
+// Base class: Car
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
 
-function SportsCar(make, model, topSpeed) {}
+  // Method to get make and model
+  getMakeModel() {
+    return `${this.make} ${this.model}`;
+  }
+}
+
+// Subclass: SportsCar
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    super(make, model); // Call the parent class constructor
+    this.topSpeed = topSpeed;
+  }
+
+  // Method to get the top speed
+  getTopSpeed() {
+    return this.topSpeed;
+  }
+}
 
 // Do not change the code below
 window.Car = Car;
